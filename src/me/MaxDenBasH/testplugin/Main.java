026bbe47b7423a23cb5dba8b972098a37526b51d
Max <maxdieters02@gmail.com>
2015-04-27 14:22:36 +0200

    commit

---
+++ src/me/MaxDenBasH/testplugin/Main.java
@@ -0,0 +1,14 @@
+package me.MaxDenBasH.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Max on 27-4-2015.
+ */
+public class Main extends JavaPlugin {
+
+
+
+}
+
+maar tim hoe kan nu mijn vriend ook dit project bewrekeasdfadfadf
